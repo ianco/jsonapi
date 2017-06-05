cee4293f56d3e5bee073f39d5fe5d60e45952969
ianco <iancostanzo@gmail.com>
2017-06-06 00:07:26 +0100

    Add config script

---
+++ add-config.sh
@@ -0,0 +1,3 @@
+curl -H "Content-Type: application/json" -d '{"difficulty_rating": "4", "startup": {"ai_nation_count": "3", "start_up_cash": "1000", "ai_start_up_cash": "2000", "ai_aggressiveness": "3", "start_up_independent_town": "15", "start_up_raw_site": "55", "difficulty_level": "4" }}' http://localhost:8080/config
+
+
